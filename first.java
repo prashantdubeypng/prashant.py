b9080669c8e775f6cf6b2f2f9463f1e3b6a47f86
prashantdubeypng <143105656+prashantdubeypng@users.noreply.github.com>
2023-11-14 08:10:09 +0000

    Pending changes exported from your codespace

---
+++ first.java
@@ -0,0 +1,8 @@
+
+
+class HelloWorld {
+    public static void main(string[] arg){
+         System.out.println("hello world!");
+    }
+    
+}
--- prashant.py
+++ prashant.py
@@ -1,1 +1,7 @@
-import natural 
+import google.auth
+
+# Use google.auth to get the default credentials
+credentials, project = google.auth.default()
+
+# Now you can use the credentials to authenticate with Google services
+# For example, you might use these credentials to access a Google Cloud API
